c14eb4463e3184cec5956a4be46fe19714a71252
Space-Belt <dngurwn@gmail.com>
2022-03-01 01:15:41 +0900

    [Feat] 텔레포트, Slots, Event Modifiers, Emitting Custom Event, Props 기능공부완료

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,56 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal />
+  <p>Welcome...</p>
+
+  <teleport to="#modals" v-if="showModal">
+    <ModalPage theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>SpaceBelt Givaway</h1>
+      <p>Space-belt Space-belt Space-belt Space-belt</p>
+    </ModalPage>
+  </teleport>
+
+  <teleport to="#modals" v-if="showModalTwo">
+    <ModalPage @close="toggleModalTwo">
+      <h1>SpaceBelt Givaway</h1>
+      <p>Crucial-Sub Crucial-Sub Crucial-Sub Crucial-Sub Crucial-Sub</p>
+    </ModalPage>
+  </teleport>
+
+  <button @click.alt="toggleModal">open modal (alt)</button>
+  <button @click="toggleModalTwo">open modal 2</button>
 </template>
 
 <script>
-import Modal from "./components/Modal";
+import ModalPage from "./components/ModalPage.vue";
 
 export default {
   name: "App",
-  components: { Modal },
+  components: { ModalPage },
   data() {
     return {
       title: "My First Vue App :)",
+      showModal: false,
+      showModalTwo: false,
     };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
   },
 };
 </script>
 
 <style>
-#app {
+#app,
+#modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -30,7 +62,5 @@ h1 {
   border-bottom: 1px solid #ddd;
   display: inline-block;
   padding-bottom: 10px;
-}
-.active {
 }
 </style>
